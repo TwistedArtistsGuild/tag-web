aad1a11c60617894b5e072c34e97eff6afd5ccad
Bobb Shields <3019404+xiebozhi@users.noreply.github.com>
2025-07-09 21:12:19 -0500

    refactor - manually adding & editing new open source header that linter missed & messed up

--- libs/stripe.js
+++ libs/stripe.js
@@ -1,4 +1,14 @@
-import Stripe from "stripe";
+/* This file is part of the Twisted Artists Guild project.
+
+ Copyright (C) 2025 Twisted Artists Guild
+
+ Licensed under the GNU General Public License v3.0
+ (https://www.gnu.org/licenses/gpl-3.0.en.html).
+
+ This software comes with NO WARRANTY; see the license for details.
+
+ Open source · low-profit · human-first*/
+ import Stripe from "stripe";
 
 // This is the code for the createCheckout function for one-time payments (and save data for later of needed)
 export const createCheckout = async ({
