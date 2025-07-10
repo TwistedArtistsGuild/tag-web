3df5340bfdfd139cab10c05a030907bd74f08d2e
Bobb Shields <3019404+xiebozhi@users.noreply.github.com>
2025-07-09 21:06:14 -0500

    processing resignations from board, and adding open source header

--- content/us/profiles.js
+++ content/us/profiles.js
@@ -1,10 +1,17 @@
-import bobbProfile from "./bobb";
+/* This file is part of the Twisted Artists Guild project.
+
+ Copyright (C) 2025 Twisted Artists Guild
+
+ Licensed under the GNU General Public License v3.0
+ (https://www.gnu.org/licenses/gpl-3.0.en.html).
+
+ This software comes with NO WARRANTY; see the license for details.
+
+ Open source · low-profit · human-first*/
+ import bobbProfile from "./bobb";
 import satyaProfile from "./satya";
-//import vickiProfile from "./vicki";
 import turleProfile from "./turle";
 import baileyProfile from "./bailey";
-//import echoProfile from "./echo";
-import aprilProfile from "./april";
 import jasonProfile from "./jason";
 import meganProfile	from "./megan";
 
