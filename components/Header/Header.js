8e243341d25a92c54760d094562121af6bc6a292
Bobb Shields <3019404+xiebozhi@users.noreply.github.com>
2025-07-16 21:08:30 -0500

    ran linter, mostly header fixes

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,11 +1,14 @@
-"use client"
-
 /* This file is part of the Twisted Artists Guild project.
+
  Copyright (C) 2025 Twisted Artists Guild
+
  Licensed under the GNU General Public License v3.0
  (https://www.gnu.org/licenses/gpl-3.0.en.html).
+
  This software comes with NO WARRANTY; see the license for details.
+
  Open source · low-profit · human-first*/
+"use client"
 
 import { useAppContext } from "/components/Context"
 import { useState, useEffect } from "react"
