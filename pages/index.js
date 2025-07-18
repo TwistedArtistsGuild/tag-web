3edfbc09b6be9c73a697786e815dfa961b3cb85b
Bobb Shields <3019404+xiebozhi@users.noreply.github.com>
2025-07-17 21:43:54 -0500

    another weird bug after a merge

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,3 @@
-"use client"
-
-import React from "react"
-
 /* This file is part of the Twisted Artists Guild project.
  Copyright (C) 2025 Twisted Artists Guild
  Licensed under the GNU General Public License v3.0
