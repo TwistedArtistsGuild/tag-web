48f2fc07716265c5573dc9791d234e67d9bd6556
Bobb Shields <3019404+xiebozhi@users.noreply.github.com>
2026-04-15 22:03:09 -0500

    $65 #37 - restructure - deleting competitions and vote, changed to contests effectively.  header updates included.

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,10 +1,12 @@
-"use client"
-
 /* This file is part of the Twisted Artists Guild project.
+
  Copyright (C) 2025 Twisted Artists Guild
+
  Licensed under the GNU General Public License v3.0
  (https://www.gnu.org/licenses/gpl-3.0.en.html).
+
  This software comes with NO WARRANTY; see the license for details.
+
  Open source · low-profit · human-first*/
 "use client"
 
@@ -262,12 +264,12 @@ export default function Header({ pageSections = [] }) {
               News
             </Link>
             <Link
-              href="/vote/"
-              className={`text-lg ${getTextColorClass(active === "vote")}`}
-              onClick={() => handleActive("vote")}
-              name="vote"
+              href="/contests/"
+              className={`text-lg ${getTextColorClass(active === "contests")}`}
+              onClick={() => handleActive("contests")}
+              name="contests"
             >
-              Vote
+              Contests
             </Link>
           </nav>
 
